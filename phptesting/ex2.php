52b39dfeb31a4e983f954b90521f2a623be781f8
sitnyein <chankhinem@gmail.com>
2023-08-16 15:07:12 -0400

    day2 commit

---
+++ phptesting/ex2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    strlen()
+    str_word_count()
+    strrev()
+    str_shuffle()
+    strpos(str,search)
+    substr(str,start,end)
+    strtoupper()
+    strtolower()
+    strip_tags()
+    str_replace(find,new,string)
+    trim()
+    str_split()
+    explode()
+    
+    <?php 
+     $name = 'si thu';
+     echo "<br>";
+     echo strtoupper($name);
+    ?>
+</body>
+</html>
