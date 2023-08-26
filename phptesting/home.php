ead8663d0a254f5af437509c4f87a1e73c6e965c
sitnyein <chankhinem@gmail.com>
2023-08-26 23:19:27 +0630

    day one commit

--- phptesting/home.php
+++ phptesting/home.php
@@ -10,7 +10,12 @@
     <a href="./ex1.php">youtube</a>
     <h1>home page</h1>
     <?php
+    session_start();
+    $_SESSION['name'] = "sesssion mgmg"; //session create
      echo "hello world this is home page";
+     $name = "mgmg";
+     //cookie create 
+     setcookie('phone','09999999',time()+ 10);
     ?>
 </body>
 </html>
