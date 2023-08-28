3b4f25487bd91493096a2661b3368ea4755f4ab1
sitnyein <chankhinem@gmail.com>
2023-08-29 00:24:16 +0630

    day twob  commit

--- phptesting/classobject.php
+++ phptesting/classobject.php
@@ -139,3 +139,28 @@ echo $dog->color;
 // echo $obj2->private;  // Cannot access private property
 // $obj2->Display(); //Displays all properties
 ?>
+
+<!-- __construct -->
+<?php
+class Fruit {
+  public $name;
+  public $color;
+
+  function __construct($name, $color) {
+    $this->name = $name;
+    $this->color = $color;
+    echo "********this is auto function";
+  }
+  function get_name() {
+    return $this->name;
+  }
+  function get_color() {
+    return $this->color;
+  }
+}
+
+$apple = new Fruit("Apple", "red");
+echo $apple->get_name();
+echo "<br>";
+echo $apple->get_color();
+?>
