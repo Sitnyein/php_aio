1b1512e4866e1775aa8454575b9b545080766a9d
sitnyein <chankhinem@gmail.com>
2023-09-10 23:46:04 +0630

    day fivea commit

--- phptesting/todolist/create.php
+++ phptesting/todolist/create.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
 </head>
 <body>
  <h3 class="mt-3"> <a href="./read.php"> List read</a></h3>
@@ -25,23 +26,23 @@
   </div>
 </form>
     <?php
-    require_once('./dbconnect.php');
-    if(isset($_POST['create'])) {
-        $name = $_POST['uname'];
-        $town = $_POST['utown'];
-        if($name == null || $town == null ){
-            echo "<h1>you need to fill</h1>";
-       }
-       else { 
+    // require_once('./dbconnect.php');
+    // if(isset($_POST['create'])) {
+    //     $name = $_POST['uname'];
+    //     $town = $_POST['utown'];
+    //     if($name == null || $town == null ){
+    //         echo "<h1>you need to fill</h1>";
+    //    }
+    //    else { 
         
-     $sql =  "insert into todo (name,town) values ('$name','$town')";
-     if(mysqli_query($con,$sql)) {
-         echo "success";
-     } else { echo "error";}
+    //  $sql =  "insert into todo (name,town) values ('$name','$town')";
+    //  if(mysqli_query($con,$sql)) {
+    //      echo "success";
+    //  } else { echo "error";}
      
-       }
+    //    }
  
-    }
+    // }
 
     ?>
 
@@ -50,4 +51,5 @@
 
 
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </html>
